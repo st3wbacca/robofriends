b6f8e809992e48a121c5d6d2d24f650fc9a8ed74
Stuart B <stuart.busilla@gmail.com>
2018-12-12 15:28:48 -0600

    add search ability to app

--- src/App.js
+++ src/App.js
@@ -4,12 +4,28 @@ import SearchBox from './SearchBox';
 import { robots } from './robots'
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: robots,
+      searchField: '',
+    }
+  };
+
+  onSearchChange = (event) => {
+    this.setState({ searchField: event.target.value });
+  };
+
   render() {
+    const filteredRobots = this.state.robots.filter(robot => {
+      return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+    });
+    
     return (
       <div className='tc'>
         <h1>RoboFriends</h1>
-        <SearchBox />
-        <CardList robots={robots} />
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList robots={filteredRobots} />
       </div>
     );
   }
