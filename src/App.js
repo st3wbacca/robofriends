16020fc54d6a7ba15619c3f6ce15f70d2285cf9b
Stuart B <stuart.busilla@gmail.com>
2018-12-12 15:05:38 -0600

    center app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
+import SearchBox from './SearchBox';
 import { robots } from './robots'
 
 class App extends Component {
   render() {
     return (
-      <CardList robots={robots} />
+      <div className='tc'>
+        <h1>RoboFriends</h1>
+        <SearchBox />
+        <CardList robots={robots} />
+      </div>
     );
   }
 }
