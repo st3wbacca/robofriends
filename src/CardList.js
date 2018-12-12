8fe48aa3df331837908411df657681e523f8b1b4
Stuart B <stuart.busilla@gmail.com>
2018-12-12 14:54:16 -0600

    fix formatting

--- src/CardList.js
+++ src/CardList.js
@@ -3,12 +3,14 @@ import Card from './Card';
 
 const CardList = ({ robots }) => {
 	const cardsArray = robots.map((user, i) => {
-		return <Card
-			key={robots[i].id}
-      id={robots[i].id}
-      name={robots[i].name}
-      email={robots[i].email}
-  	/>
+		return (
+			<Card
+				key={robots[i].id}
+	      id={robots[i].id}
+	      name={robots[i].name}
+	      email={robots[i].email}
+	  	/>
+		);
 	});
 
 	return (
