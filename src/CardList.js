99670eb87169b8af4519e152fc9e5fb7a06c31a8
Stuart B <stuart.busilla@gmail.com>
2018-12-12 14:52:50 -0600

    add key prop for array

--- src/CardList.js
+++ src/CardList.js
@@ -4,6 +4,7 @@ import Card from './Card';
 const CardList = ({ robots }) => {
 	const cardsArray = robots.map((user, i) => {
 		return <Card
+			key={robots[i].id}
       id={robots[i].id}
       name={robots[i].name}
       email={robots[i].email}
