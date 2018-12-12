75af09120ca73da9d25f2767ba3c42ecd6af8f2c
Stuart B <stuart.busilla@gmail.com>
2018-12-12 14:49:58 -0600

    display to list of cards

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React, { Component } from 'react';
-import Card from './Card';
+import CardList from './CardList';
 import { robots } from './robots'
 
 class App extends Component {
   render() {
     return (
-      <Card
-        id={robots[0].id}
-        name={robots[0].name}
-        email={robots[0].email}
-      />
+      <CardList robots={robots} />
     );
   }
 }
