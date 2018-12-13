826d02df869adffe08ddee4598afd3be29ef5f18
Stuart B <stuart.busilla@gmail.com>
2018-12-13 11:17:01 -0600

    Updates

---
+++ precache-manifest.e718788e5d624097e45ffd630b9328b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "1fbda54f8e12fb27253f",
+    "url": "/robofriends/static/js/main.1fbda54f.chunk.js"
+  },
+  {
+    "revision": "bf07838d016402ca8345",
+    "url": "/robofriends/static/js/1.bf07838d.chunk.js"
+  },
+  {
+    "revision": "1fbda54f8e12fb27253f",
+    "url": "/robofriends/static/css/main.ec1b433a.chunk.css"
+  },
+  {
+    "revision": "bf07838d016402ca8345",
+    "url": "/robofriends/static/css/1.b800399d.chunk.css"
+  },
+  {
+    "revision": "5f1d3863770311678725d023a5f4e89a",
+    "url": "/robofriends/index.html"
+  }
+];
