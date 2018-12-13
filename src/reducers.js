03f2ddd4f049e4cf86635b225e5c54cb02e34a15
Stuart B <stuart.busilla@gmail.com>
2018-12-13 16:44:51 -0600

    fix formatting

--- src/reducers.js
+++ src/reducers.js
@@ -8,9 +8,6 @@ export const searchRobots = (state=initialState, action={}) => {
 	switch(action.type) {
 		case CHANGE_SEARCH_FIELD:
 			return Object.assign({}, state, { searchField: action.payload });
-			break;
 		default:
 			return state;
-			break;
-	};
-};
+}};
