c0c86496e7b5966033c4f21193ae9830744efea7
Stuart B <stuart.busilla@gmail.com>
2019-02-07 15:41:39 -0600

    add test for shouldComponentUpdate

--- src/components/CounterButton.test.js
+++ src/components/CounterButton.test.js
@@ -1,4 +1,5 @@
 import { shallow } from 'enzyme';
+import sinon from 'sinon';
 import '../setupTests';
 import React from 'react';
 import CounterButton from './CounterButton';
@@ -23,4 +24,27 @@ it('correctly increments the counter', () => {
 	wrapper.find('[id="counter"]').simulate('keypress');
 	expect(wrapper.state()).toEqual({ count: 3 });
 	expect(wrapper.props().color).toEqual('red');
+});
+
+it('only updates if the count state changes, not color prop', () => {
+	expect.assertions(9);
+	const mockColor = 'red';
+	const spy = sinon.spy(CounterButton.prototype, 'shouldComponentUpdate');
+	const wrapper = shallow(<CounterButton color={mockColor} />);
+	
+	expect(wrapper.state()).toEqual({ count: 0 });
+	expect(spy.callCount).toEqual(0);
+	expect(spy.notCalled).toBeTruthy();
+	
+	wrapper.find('[id="counter"]').simulate('click');
+	expect(spy.callCount).toEqual(1);
+	expect(spy.returnValues[0]).toBeTruthy();
+	// or
+	expect(spy.getCall(0).returned(true)).toBeTruthy();
+
+	wrapper.setProps({ color: 'blue'});
+	expect(spy.callCount).toEqual(2);
+	expect(spy.returnValues[1]).toBeFalsy();
+	// or
+	expect(spy.getCall(1).returned(false)).toBeTruthy();
 });
